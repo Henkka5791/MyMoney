c2b6de3b7794ed10f2eabc47681f8ef7d4b48c8d
Henkka5791 <henrik.hamalainen@gmail.com>
2020-12-18 20:48:19 +0200

    Add login, logout, registeration

--- routes.py
+++ routes.py
@@ -1,6 +1,34 @@
 from app import app
-from flask import render_template
+from flask import redirect, render_template, request, session
+import accounts
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    username = accounts.get_username()
+    return render_template("index.html", username = username)
+
+@app.route("/login",methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+    if accounts.login(username, password):
+        return redirect("/")
+    else: 
+       return render_template("error.html", error = "Väärä tunnus tai salasana")
+
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if accounts.register(username,password):
+            return redirect("/")
+        else:
+            return render_template("register.html",error_message="Rekisteröinti ei onnistunut")
+
+@app.route("/logout")
+def logout():
+    accounts.logout()
+    return redirect("/")
