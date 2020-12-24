e970759827fc59c9152a0c12913fe563ce95c564
Henkka5791 <henrik.hamalainen@gmail.com>
2020-12-24 11:00:38 +0200

    Add view_one

--- transactions.py
+++ transactions.py
@@ -15,7 +15,7 @@ def add(subcategory_id,amount,description):
         return False
 
 def list():
-    sql = "SELECT created_at, amount, description FROM transactions WHERE visible=1"
+    sql = "SELECT t.created_at,t.amount,c.name,s.name,t.description, t.id FROM transactions t, categories c, subcategories s WHERE t.visible=1 AND c.visible = 1 AND s.visible = 1 AND t.subcategory_id=s.id AND s.category_id=c.id ORDER BY t.created_at DESC LIMIT 10"
     result = db.session.execute(sql)
     transactions = result.fetchall()
     return transactions
@@ -23,7 +23,15 @@ def list():
 def is_outcome(subcategory_id):
     sql = "SELECT c.outcome FROM categories c, subcategories s WHERE s.id=:subcategory_id AND c.id=s.category_id"
     result = db.session.execute(sql, {"subcategory_id":subcategory_id})
-    outcome = result.fetchone()[0];
+    outcome = result.fetchone()[0]
     if outcome == 1:
         return True
     return False
+
+def view_one(id):
+    print(id)
+    sql = "SELECT t.created_at,t.amount,t.description,t.id,c.name,s.name,p.data FROM categories c, subcategories s,transactions t LEFT JOIN pictures p ON p.id=t.picture_id WHERE t.id=:id AND t.subcategory_id=s.id AND c.id = s.category_id"
+    result = db.session.execute(sql,{"id":id})
+    transaction = result.fetchone()
+    print(transaction[0])
+    return transaction 
