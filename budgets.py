7c9e9ac70451cf42bf9d072258e3d1e7eab34dba
Henkka5791 <henrik.hamalainen@gmail.com>
2020-12-27 19:02:06 +0200

    Add budget_edit

--- budgets.py
+++ budgets.py
@@ -43,3 +43,25 @@ def category_not_in_budget(year,category_id):
     if is_category == None:
         return True
     return False
+
+def budget_list(year):
+    account_id = accounts.user_id()
+    period_start = str(year)+"-"+"1"+"-"+"1"
+    period_end = str(year)+"-"+"12"+"-"+"2"
+    sql = "SELECT c.name,b.amount,extract(month FROM b.period) AS month,b.id FROM categories c, budgets b WHERE c.account_id=:account_id AND c.id=b.category_id AND b.period>=:period_start AND b.period<:period_end"
+    result = db.session.execute(sql,{"account_id":account_id,"period_start":period_start,"period_end":period_end})
+    budgets = result.fetchall()
+    return budgets
+
+def budget_update(ids,amounts):
+    try:
+        for idx in enumerate(ids):
+            amount = amounts[idx[0]]
+            id = idx[1]
+            print("id: "+id+" amount "+amount)
+            sql = "UPDATE budgets SET amount=:amount WHERE id=:id"
+            db.session.execute(sql,{"amount":amount,"id":id})
+        db.session.commit()
+        return True
+    except:
+        return False
