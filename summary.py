e47ce8ba129b9b39bde2da2bd0627a1416abf654
Henkka5791 <henrik.hamalainen@gmail.com>
2021-01-03 20:27:54 +0200

    Fix sql-query and Add sql-query for total sum

--- summary.py
+++ summary.py
@@ -2,12 +2,123 @@ from db import db
 import accounts
 from calendar import monthrange
 
-def monthly(month_from,month_to,year_from,year_to):
-    time_from = str(year_from)+"-"+str(month_from)+"-"+"1"
+def monthly(day_from,day_to):
+    id = accounts.user_id()
+    time_from = day_from+"-"+"01"
+    parts = day_to.split("-")
+    year_to = int(parts[0])
+    month_to = int(parts[1])
     last_day = monthrange(year_to,month_to)
-    time_to = str(year_to)+"-"+str(month_to)+"-"+str(last_day[1])
+    time_to = day_to+"-"+str(last_day[1])
     visible = 1
-    sql = "SELECT EXTRACT(year FROM t.created_at),EXTRACT(month FROM t.created_at),ROUND(SUM(CASE WHEN t.amount < 0 THEN t.amount ELSE 0 END)::numeric,2),ROUND(SUM(CASE WHEN t.amount > 0 THEN t.amount ELSE 0 END)::numeric,2),ROUND(SUM(t.amount)::numeric,2),a.amount FROM transactions t, (SELECT b.period AS period,SUM(b.amount) AS amount FROM budgets b GROUP BY 1) AS a WHERE EXTRACT(year FROM t.created_at)=EXTRACT(year FROM period) AND EXTRACT(month FROM t.created_at)=EXTRACT(month FROM period) AND t.created_at>=:time_from AND t.created_at<=:time_to AND t.visible=:visible GROUP BY 1,2,6;"
-    result = db.session.execute(sql,{"time_from":time_from,"time_to":time_to,"visible":visible})
+    sql = '''SELECT 
+                b.year::numeric::integer,
+                b.month::numeric::integer,
+                COALESCE(t.income,0),
+                COALESCE(t.outcome,0),
+                COALESCE(b.budget,0),
+                COALESCE(t.income+t.outcome,0) AS balance,
+                COALESCE(b.budget+t.outcome,0) AS budget_balance
+            FROM 
+                (SELECT 
+                    EXTRACT(year FROM t.created_at) AS year,
+                    EXTRACT(month FROM t.created_at) AS month,
+                    ROUND(SUM(CASE WHEN t.amount > 0 THEN t.amount ELSE 0 END)::numeric,2) AS income,
+                    ROUND(SUM(CASE WHEN t.amount < 0 THEN t.amount ELSE 0 END)::numeric,2) AS outcome 
+                FROM    
+                    transactions t,
+                    subcategories s,
+                    categories c 
+                WHERE 
+                    t.subcategory_id=s.id
+                    AND s.category_id=c.id 
+                    AND c.account_id=:id 
+                    AND t.visible=:visible
+                    AND s.visible=:visible 
+                    AND c.visible=:visible 
+                GROUP BY 
+                    1,2) AS t 
+            FULL OUTER JOIN (
+                SELECT 
+                    EXTRACT(year FROM b.period) AS year,
+                    EXTRACT(month FROM b.period) AS month, 
+                    b.period AS period,
+                    sum(b.amount) AS budget from budgets b, 
+                    categories c 
+                WHERE 
+                    b.category_id=c.id 
+                    AND c.account_id=:id 
+                    AND c.visible=:visible 
+                    GROUP BY 1,2,3) AS b 
+            ON 
+                t.year=b.year 
+                AND t.month=b.month 
+            WHERE 
+                b.period>=:time_from 
+                AND b.period<=:time_to'''
+    result = db.session.execute(sql,{"time_from":time_from,"time_to":time_to,"visible":visible,"id":id})
     monthly_results = result.fetchall()
     return monthly_results
+
+def total_sum(day_from,day_to):
+    id = accounts.user_id()
+    time_from = day_from+"-"+"01"
+    parts = day_to.split("-")
+    year_to = int(parts[0])
+    month_to = int(parts[1])
+    last_day = monthrange(year_to,month_to)
+    time_to = day_to+"-"+str(last_day[1])
+    visible = 1
+    sql = '''SELECT 
+                count(b.month),
+                sum(COALESCE(t.income,0)),
+                sum(COALESCE(t.outcome,0)),
+                sum(COALESCE(b.budget,0)),
+                sum(COALESCE(t.income+t.outcome,0)) AS balance,
+                sum(COALESCE(b.budget+t.outcome,0)) AS budget_balance
+            FROM 
+                (SELECT 
+                    EXTRACT(year FROM t.created_at) AS year,
+                    EXTRACT(month FROM t.created_at) AS month,
+                    ROUND(SUM(CASE WHEN t.amount > 0 THEN t.amount ELSE 0 END)::numeric,2) AS income,
+                    ROUND(SUM(CASE WHEN t.amount < 0 THEN t.amount ELSE 0 END)::numeric,2) AS outcome 
+                FROM    
+                    transactions t,
+                    subcategories s,
+                    categories c 
+                WHERE 
+                    t.subcategory_id=s.id
+                    AND s.category_id=c.id 
+                    AND c.account_id=:id 
+                    AND t.visible=:visible
+                    AND s.visible=:visible 
+                    AND c.visible=:visible 
+                GROUP BY 
+                    1,2) AS t 
+            FULL OUTER JOIN (
+                SELECT 
+                    EXTRACT(year FROM b.period) AS year,
+                    EXTRACT(month FROM b.period) AS month, 
+                    b.period AS period,
+                    sum(b.amount) AS budget from budgets b, 
+                    categories c 
+                WHERE 
+                    b.category_id=c.id 
+                    AND c.account_id=:id 
+                    AND c.visible=:visible 
+                    GROUP BY 1,2,3) AS b 
+            ON 
+                t.year=b.year 
+                AND t.month=b.month 
+            WHERE 
+                b.period>=:time_from 
+                AND b.period<=:time_to'''
+    result = db.session.execute(sql,{"time_from":time_from,"time_to":time_to,"visible":visible,"id":id})
+    total = result.fetchone()
+    return total
+
+#select EXTRACT(year FROM t.created_at) AS year, EXTRACT(month FROM t.created_at) AS month, ROUND(SUM(CASE WHEN t.amount > 0 THEN t.amount ELSE 0 END)::numeric,2) AS income,ROUND(SUM(CASE WHEN t.amount < 0 THEN t.amount ELSE 0 END)::numeric,2) AS outcome FROM transactions t, subcategories s, categories c WHERE t.subcategory_id=s.id AND s.category_id=c.id AND c.account_id=3 AND t.visible=1 AND s.visible=1 AND c.visible=1 GROUP BY 1,2;
+
+#select EXTRACT(year FROM b.period) AS year,EXTRACT(month FROM b.period) AS month, sum(b.amount) AS budget from budgets b, categories c WHERE b.category_id=c.id AND c.account_id=3 AND c.visible=1 GROUP BY 1,2; 
+
+#select b.year,b.month,t.income,t.outcome,b.budget,t.income+t.outcome AS balance,t.outcome+b.budget AS budget_balance FROM (select EXTRACT(year FROM t.created_at) AS year, EXTRACT(month FROM t.created_at) AS month, ROUND(SUM(CASE WHEN t.amount > 0 THEN t.amount ELSE 0 END)::numeric,2) AS income,ROUND(SUM(CASE WHEN t.amount < 0 THEN t.amount ELSE 0 END)::numeric,2) AS outcome FROM transactions t, subcategories s, categories c WHERE t.subcategory_id=s.id AND s.category_id=c.id AND c.account_id=3 AND t.visible=1 AND s.visible=1 AND c.visible=1 GROUP BY 1,2) AS t FULL OUTER JOIN (select EXTRACT(year FROM b.period) AS year,EXTRACT(month FROM b.period) AS month, b.period AS period,sum(b.amount) AS budget from budgets b, categories c WHERE b.category_id=c.id AND c.account_id=3 AND c.visible=1 GROUP BY 1,2,3) AS b ON t.year=b.year AND t.month=b.month WHERE b.period>='2020-11-1' AND b.period<='2021-1-31'
