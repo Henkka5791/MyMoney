4e854d007c515840f651eb3d8ce079f7ce36eb94
Henkka5791 <henrik.hamalainen@gmail.com>
2021-01-04 20:45:54 +0200

    Refactor sql-query

--- budgets.py
+++ budgets.py
@@ -48,8 +48,23 @@ def budget_list(year):
     account_id = accounts.user_id()
     period_start = str(year)+"-"+"1"+"-"+"1"
     period_end = str(year)+"-"+"12"+"-"+"2"
-    sql = "SELECT c.name,b.amount,extract(month FROM b.period) AS month,b.id FROM categories c, budgets b WHERE c.account_id=:account_id AND c.id=b.category_id AND b.period>=:period_start AND b.period<:period_end ORDER BY c.name,month ASC"
-    result = db.session.execute(sql,{"account_id":account_id,"period_start":period_start,"period_end":period_end})
+    visible = 1
+    sql = '''SELECT 
+                c.name,
+                b.amount,
+                extract(month FROM b.period) AS month,
+                b.id 
+            FROM 
+                categories c, 
+                budgets b 
+            WHERE 
+                c.account_id=:account_id 
+                AND c.id=b.category_id 
+                AND b.period>=:period_start 
+                AND b.period<:period_end 
+                AND c.visible=:visible 
+            ORDER BY c.name,month ASC'''
+    result = db.session.execute(sql,{"account_id":account_id,"period_start":period_start,"period_end":period_end,"visible":visible})
     budgets = result.fetchall()
     return budgets
 
@@ -58,7 +73,6 @@ def budget_update(ids,amounts):
         for idx in enumerate(ids):
             amount = amounts[idx[0]]
             id = idx[1]
-            print("id: "+id+" amount "+amount)
             sql = "UPDATE budgets SET amount=:amount WHERE id=:id"
             db.session.execute(sql,{"amount":amount,"id":id})
         db.session.commit()
@@ -69,7 +83,18 @@ def budget_update(ids,amounts):
 def budget_sum(budget_year):
     account_id = accounts.user_id()
     visible = 1
-    sql = "SELECT c.name,sum(b.amount) FROM categories c, budgets b WHERE c.account_id=:account_id AND b.category_id=c.id AND c.visible=:visible AND extract(year FROM b.period)=:budget_year GROUP BY 1"
+    sql = '''SELECT 
+                c.name,
+                sum(b.amount) 
+            FROM 
+                categories c, 
+                budgets b 
+            WHERE 
+                c.account_id=:account_id 
+                AND b.category_id=c.id 
+                AND c.visible=:visible 
+                AND extract(year FROM b.period)=:budget_year 
+            GROUP BY 1'''
     result = db.session.execute(sql,{"account_id":account_id,"visible":visible,"budget_year":budget_year})
     sums = result.fetchall()
     return sums
