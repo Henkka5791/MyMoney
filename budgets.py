1aeec2d6cd537d75a4d9f82efdab7f56077d288a
Henkka5791 <henrik.hamalainen@gmail.com>
2021-02-08 19:12:04 +0200

    Fix line lengths

--- budgets.py
+++ budgets.py
@@ -7,12 +7,13 @@ def create_budget(year):
         if year in budget_years():
             return False
         for category in category_list:
-            print(category)
             for i in range(1,13):
                 category_id = category[0]
                 period = str(year)+"-"+str(i)+"-"+"1"
-                sql = "INSERT INTO budgets(period,category_id) VALUES (:period,:category_id)"
-                db.session.execute(sql,{"period":period, "category_id":category_id})
+                sql ='''INSERT INTO budgets(period,category_id) 
+                        VALUES (:period,:category_id)'''
+                db.session.execute(sql,\
+                    {"period":period, "category_id":category_id})
         db.session.commit()
         return True
     except:
@@ -34,7 +35,8 @@ def budget_add_category(year, category_id):
         for i in range(1,13):
             period = str(year)+"-"+str(i)+"-"+"1"
             print(period)
-            sql = "INSERT INTO budgets(period,category_id) VALUES (:period,:category_id)"
+            sql ='''INSERT INTO budgets(period,category_id) 
+                    VALUES (:period,:category_id)'''
             db.session.execute(sql,{"period":period,"category_id":category_id})
         db.session.commit()
         return True
@@ -44,7 +46,8 @@ def budget_add_category(year, category_id):
 def category_not_in_budget(year, category_id):
     period = str(year)+"-"+"1"+"-"+"1"
     sql = "SELECT :category_id FROM budgets WHERE period=:period AND category_id=:category_id"
-    result = db.session.execute(sql,{"category_id":category_id, "period":period})
+    result = db.session.execute(sql,\
+        {"category_id":category_id, "period":period})
     is_category = result.fetchone()
     if is_category == None:
         return True
@@ -70,7 +73,8 @@ def budget_list(year):
                 AND b.period<:period_end 
                 AND c.visible=:visible 
             ORDER BY c.name,month ASC'''
-    result = db.session.execute(sql,{"account_id":account_id, "period_start":period_start, "period_end":period_end, "visible":visible})
+    result = db.session.execute(sql,\
+        {"account_id":account_id, "period_start":period_start, "period_end":period_end, "visible":visible})
     budgets = result.fetchall()
     return budgets
 
@@ -82,7 +86,7 @@ def budget_update(ids, amounts):
             amount = amount.replace("-","")
             id = idx[1]
             sql = "UPDATE budgets SET amount=:amount WHERE id=:id"
-            db.session.execute(sql,{"amount":amount, "id":id})
+            db.session.execute(sql, {"amount":amount, "id":id})
         db.session.commit()
         return True
     except:
@@ -103,6 +107,7 @@ def budget_sum(budget_year):
                 AND c.visible=:visible 
                 AND extract(year FROM b.period)=:budget_year 
             GROUP BY 1'''
-    result = db.session.execute(sql,{"account_id":account_id, "visible":visible, "budget_year":budget_year})
+    result = db.session.execute(sql,\
+        {"account_id":account_id, "visible":visible, "budget_year":budget_year})
     sums = result.fetchall()
     return sums
