bda7b9c145a1fc46954e6e79c6893c54c746414f
Henkka5791 <henrik.hamalainen@gmail.com>
2021-02-08 19:12:46 +0200

    Fix line lengths

--- categories.py
+++ categories.py
@@ -5,8 +5,14 @@ def income_categories():
     account_id = accounts.user_id()
     visible = 1
     outcome = 0
-    sql = "SELECT id,name FROM categories WHERE account_id=:account_id AND visible=:visible AND outcome=:outcome"
-    result = db.session.execute(sql, {"account_id":account_id, "visible":visible, "outcome":outcome})
+    sql ='''SELECT id,name 
+            FROM categories 
+            WHERE 
+                account_id=:account_id AND 
+                visible=:visible AND 
+                outcome=:outcome'''
+    result = db.session.execute(sql,\
+        {"account_id":account_id, "visible":visible, "outcome":outcome})
     categories_income = result.fetchall()
     return categories_income
 
@@ -14,16 +20,25 @@ def outcome_categories():
     account_id = accounts.user_id()
     visible = 1
     outcome = 1
-    sql = "SELECT id,name FROM categories WHERE account_id=:account_id AND visible=:visible AND outcome=:outcome"
-    result = db.session.execute(sql, {"account_id":account_id, "visible":visible, "outcome":outcome})
+    sql ='''SELECT id,name 
+            FROM categories 
+            WHERE 
+                account_id=:account_id AND 
+                visible=:visible AND 
+                outcome=:outcome'''
+    result = db.session.execute(sql,\
+        {"account_id":account_id, "visible":visible, "outcome":outcome})
     categories_outcome = result.fetchall()
     return categories_outcome
 
 def category_list():
     account_id = accounts.user_id()
     visible = 1
-    sql = "SELECT id,name FROM categories WHERE account_id=:account_id AND visible=:visible"
-    result = db.session.execute(sql, {"account_id":account_id, "visible":visible})
+    sql ='''SELECT id,name 
+            FROM categories 
+            WHERE account_id=:account_id AND visible=:visible'''
+    result = db.session.execute(sql,\
+        {"account_id":account_id, "visible":visible})
     categories = result.fetchall()
     return categories
 
@@ -33,7 +48,8 @@ def category_subcategory_list_all():
     sql ='''SELECT c.name,sc.name,sc.id 
             FROM categories c, subcategories sc 
             WHERE c.account_id=:account_id AND c.id=sc.category_id AND c.visible=:visible AND sc.visible=:visible'''
-    result = db.session.execute(sql, {"account_id":account_id, "visible":visible})
+    result = db.session.execute(sql,\
+        {"account_id":account_id, "visible":visible})
     category_subcategory_list = result.fetchall()
     return category_subcategory_list
 
@@ -42,8 +58,10 @@ def add_category(name, outcome):
     if in_categories(name, account_id):
         return False
     try:
-        sql = "INSERT INTO categories (name, outcome, account_id) VALUES (:name,:outcome,:account_id) RETURNING ID"
-        result = db.session.execute(sql, {"name":name, "outcome":outcome, "account_id":account_id})
+        sql ='''INSERT INTO categories (name, outcome, account_id) 
+                VALUES (:name,:outcome,:account_id) RETURNING ID'''
+        result = db.session.execute(sql,\
+            {"name":name, "outcome":outcome, "account_id":account_id})
         category_id = result.fetchone()[0]
         years = budgets.budget_years()
         for year in years:
@@ -57,8 +75,11 @@ def add_category(name, outcome):
 
 def subcategory_list(category_id):
     visible = 1
-    sql = "SELECT id, name FROM subcategories WHERE category_id=:category_id AND visible=:visible"
-    result = db.session.execute(sql, {"category_id":category_id, "visible":visible})
+    sql ='''SELECT id, name 
+            FROM subcategories 
+            WHERE category_id=:category_id AND visible=:visible'''
+    result = db.session.execute(sql,\
+        {"category_id":category_id, "visible":visible})
     subcategories = result.fetchall()
     return subcategories
 
@@ -77,7 +98,8 @@ def add_subcategory(name, category_id):
     if in_subcategory(name, account_id):
         return False
     try:
-        sql = "INSERT INTO subcategories(name,category_id) VALUES (:name,:category_id)"
+        sql ='''INSERT INTO subcategories(name,category_id) 
+                VALUES (:name,:category_id)'''
         db.session.execute(sql, {"name":name, "category_id":category_id})
         db.session.commit()
         return True
@@ -106,7 +128,11 @@ def subcategory_remove(id):
 
 def in_categories(name, id):
     visible = 1
-    sql ='''SELECT 1 from categories WHERE UPPER(name)=UPPER(:name) AND account_id=:id AND visible=:visible'''
+    sql ='''SELECT 1 from categories 
+            WHERE 
+                UPPER(name)=UPPER(:name) AND 
+                account_id=:id AND 
+                visible=:visible'''
     result = db.session.execute(sql, {"name":name, "id":id, "visible":visible})
     if result.fetchone() != None:
         return True
